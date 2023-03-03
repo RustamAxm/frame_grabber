966a80718fddd02a508032d93aa98f2c969b6009
lct-lab-user <lct-lab-user@1440.space>
2023-03-03 18:08:01 +0300

    dir impl

--- README.md
+++ README.md
@@ -17,13 +17,16 @@ poetry run python <script_name>
 In [grabber_example](grabber_example.py) simple image plot
 ### Unpack raw data
 Helpful util for unpacking raw data to raw [grabber_unpack](grabber_unpack.py)
-```commandline
-PS C:\Users\user\Downloads\frame_grabber> python .\grabber_unpack.py "name_packed_raw_data.raw"
+```
+python .\grabber_unpack.py source_dir out_dir
+```
+example:
+```
+PS C:\Users\user\Downloads\frame_grabber> python .\grabber_unpack.py raw_data unpacked
 [2023.03.03 14:56:15] modules I device ID = 61250064
 [2023.03.03 14:56:15] modules I GetDMAAccessEx OK
 [2023.03.03 14:56:15] Unpack RAW I in buffer len = 2949120, unpacked buffer len = 4718592
 [2023.03.03 14:56:15] Unpack RAW I raw data saved raw_data\Азимут\Без дополнительного фильтра\Поле приемного СЛК НС2НС10_unpacked.raw
-
 ```
 
 ### if u have problems with poetry
--- grabber_unpack.py
+++ grabber_unpack.py
@@ -5,6 +5,9 @@ Works only on Windows with an inserted card frame grabber
 import ctypes
 import sys
 import logging
+import glob
+import os
+import errno
 
 from modules.grabber import FrameGrabber
 from helpers.loggin_helper import set_logging
@@ -23,6 +26,13 @@ def read_raw_to_buffer(name):
 
 
 def save_unpacked(name, buffer):
+    if not os.path.exists(os.path.dirname(name)):
+        try:
+            os.makedirs(os.path.dirname(name))
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
     with open(name, 'wb') as file:
         file.write(buffer)
     logger.info(f'raw data saved {name}')
@@ -30,17 +40,27 @@ def save_unpacked(name, buffer):
 
 if __name__ == '__main__':
     try:
-        name = sys.argv[1]
+        source_dir = sys.argv[1]
     except IndexError:
-        name = r'raw_data/Азимут/Без дополнительного фильтра/Поле приемного СЛК НС2НС10.raw'
+        source_dir = r'raw_data'
 
     try:
-        out_name = sys.argv[2]
+        out_dir = sys.argv[2]
     except IndexError:
-        out_name = name[:-4] + '_unpacked.raw'
+        out_dir = 'unpacked'
 
-    in_buffer = read_raw_to_buffer(name)
+    dir_ = 'raw_data'
+    files = []
+    for file in glob.glob(f'{source_dir}/**/*.raw', recursive=True):
+        if file:
+            files.append(file)
+
     grab = FrameGrabber(Width=2048, Height=1152, BitDepth=10, Packed=1)
-    unpacked_buf = grab.unpack_image(in_buffer)
-    logger.info(f'in buffer len = {len(in_buffer)}, unpacked buffer len = {len(unpacked_buf)}')
-    save_unpacked(out_name, unpacked_buf)
+
+    for filename in files:
+        in_buffer = read_raw_to_buffer(filename)
+        unpacked_buf = grab.unpack_image(in_buffer)
+        logger.info(f'in buffer len = {len(in_buffer)}, unpacked buffer len = {len(unpacked_buf)}')
+        save_unpacked(out_dir + '/' + filename[:-4] + '_unpacked.raw', unpacked_buf)
+
+
