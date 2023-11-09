939acac30773b850ccb33e05de4ec368e5f3b0b4
Rustam Akhmadullin <r.akhmadullin@1440.space>
2023-11-09 10:49:29 +0300

    fix unpacker

--- grabber_unpack.py
+++ grabber_unpack.py
@@ -59,16 +59,78 @@ def unpack_raw(ibuffer, config):
     k = 0
     ibuffer = np.frombuffer(ibuffer, dtype=np.uint8)
     obuffer = np.zeros(int(config["Height"] * config["Width"] * 2), dtype=np.uint16)
-    for i in range(0, len(ibuffer) - BLOCK_SIZE, BLOCK_SIZE):
-        for j in range(0, BLOCK_SIZE, 4):
-            obuffer[k] = ((ibuffer[i + j + 1] & 0x03) << 8) | ((ibuffer[i + j + 0] >> 0) & 0xFF)
-            k += 1
-            obuffer[k] = ((ibuffer[i + j + 2] & 0x0F) << 6) | ((ibuffer[i + j + 1] >> 2) & 0x3F)
-            k += 1
-            obuffer[k] = ((ibuffer[i + j + 3] & 0x3F) << 4) | ((ibuffer[i + j + 2] >> 4) & 0x0F)
-            k += 1
-            obuffer[k] = ((ibuffer[i + j + 4] & 0xFF) << 2) | ((ibuffer[i + j + 3] >> 6) & 0x03)
-            k += 1
+    for i in range(0, len(ibuffer), BLOCK_SIZE):
+        obuffer[k] = ((ibuffer[i + 1] & 0x03) << 8) | ((ibuffer[i + 0] >> 0) & 0xFF)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 2] & 0x0F) << 6) | ((ibuffer[i + 1] >> 2) & 0x3F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 3] & 0x3F) << 4) | ((ibuffer[i + 2] >> 4) & 0x0F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 4] & 0xFF) << 2) | ((ibuffer[i + 3] >> 6) & 0x03)
+        k += 1
+
+        obuffer[k] = ((ibuffer[i + 6] & 0x03) << 8) | ((ibuffer[i + 5] >> 0) & 0xFF)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 7] & 0x0F) << 6) | ((ibuffer[i + 6] >> 2) & 0x3F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 8] & 0x3F) << 4) | ((ibuffer[i + 7] >> 4) & 0x0F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 9] & 0xFF) << 2) | ((ibuffer[i + 8] >> 6) & 0x03)
+        k += 1
+
+        obuffer[k] = ((ibuffer[i + 11] & 0x03) << 8) | ((ibuffer[i + 10] >> 0) & 0xFF)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 12] & 0x0F) << 6) | ((ibuffer[i + 11] >> 2) & 0x3F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 13] & 0x3F) << 4) | ((ibuffer[i + 12] >> 4) & 0x0F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 14] & 0xFF) << 2) | ((ibuffer[i + 13] >> 6) & 0x03)
+        k += 1
+
+        obuffer[k] = ((ibuffer[i + 16] & 0x03) << 8) | ((ibuffer[i + 15] >> 0) & 0xFF)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 17] & 0x0F) << 6) | ((ibuffer[i + 16] >> 2) & 0x3F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 18] & 0x3F) << 4) | ((ibuffer[i + 17] >> 4) & 0x0F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 19] & 0xFF) << 2) | ((ibuffer[i + 18] >> 6) & 0x03)
+        k += 1
+
+        obuffer[k] = ((ibuffer[i + 21] & 0x03) << 8) | ((ibuffer[i + 20] >> 0) & 0xFF)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 22] & 0x0F) << 6) | ((ibuffer[i + 21] >> 2) & 0x3F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 23] & 0x3F) << 4) | ((ibuffer[i + 22] >> 4) & 0x0F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 24] & 0xFF) << 2) | ((ibuffer[i + 23] >> 6) & 0x03)
+        k += 1
+
+        obuffer[k] = ((ibuffer[i + 26] & 0x03) << 8) | ((ibuffer[i + 25] >> 0) & 0xFF)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 27] & 0x0F) << 6) | ((ibuffer[i + 26] >> 2) & 0x3F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 28] & 0x3F) << 4) | ((ibuffer[i + 27] >> 4) & 0x0F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 29] & 0xFF) << 2) | ((ibuffer[i + 28] >> 6) & 0x03)
+        k += 1
+
+        obuffer[k] = ((ibuffer[i + 31] & 0x03) << 8) | ((ibuffer[i + 30] >> 0) & 0xFF)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 32] & 0x0F) << 6) | ((ibuffer[i + 31] >> 2) & 0x3F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 33] & 0x3F) << 4) | ((ibuffer[i + 32] >> 4) & 0x0F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 34] & 0xFF) << 2) | ((ibuffer[i + 33] >> 6) & 0x03)
+        k += 1
+
+        obuffer[k] = ((ibuffer[i + 36] & 0x03) << 8) | ((ibuffer[i + 35] >> 0) & 0xFF)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 37] & 0x0F) << 6) | ((ibuffer[i + 36] >> 2) & 0x3F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 38] & 0x3F) << 4) | ((ibuffer[i + 37] >> 4) & 0x0F)
+        k += 1
+        obuffer[k] = ((ibuffer[i + 39] & 0xFF) << 2) | ((ibuffer[i + 38] >> 6) & 0x03)
+        k += 1
 
     obuffer.byteswap(inplace=True)
 
